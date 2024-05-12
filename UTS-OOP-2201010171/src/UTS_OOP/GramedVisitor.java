6b28ef7a97f0fbe523f72f9ea560fc4244c3317d
Chornicles <163788497+Chornicles@users.noreply.github.com>
2024-05-12 11:07:18 +0800

    Add files via upload

---
+++ UTS-OOP-2201010171/src/UTS_OOP/GramedVisitor.java
@@ -0,0 +1,21 @@
+package UTS_OOP;
+
+/**
+ *
+ * @author Gung Ardi
+ */
+public class GramedVisitor {
+    private String nama;
+
+    public GramedVisitor(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
